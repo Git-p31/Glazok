438c2d1e49ea4abe04496d88e474488cc979780d
Git-p31 <165892798+Git-p31@users.noreply.github.com>
2026-08-15 12:26:02 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+﻿const { app, BrowserWindow } = require('electron');
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    webPreferences: { webSecurity: false }
+  });
+  win.setMenuBarVisibility(false);
+  win.loadFile('index.html');
+}
+app.whenReady().then(createWindow);
